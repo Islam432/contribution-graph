00c2ba44194eaa102cc981c9a43330b8d248975c
Islam Temirgaliev <islam2004tr@mail.ru>
2023-08-12 14:46:04 +0600

    create color days

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css'
 import ContributionGraph from './components/ContributionGraph/ContributionGraph';
 
 const App: React.FC = () => {
+
   return (
     <div className="App">
       <h1>Contribution Graph</h1>
+
       <ContributionGraph />
     </div>
   );
--- src/components/ContributionGraph/ContributionGraph.tsx
+++ src/components/ContributionGraph/ContributionGraph.tsx
@@ -22,36 +22,59 @@ const ContributionGraph: React.FC = () => {
     fetchContributions();
   }, []);
 
-//   const daysOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 
   const today: Date = new Date();
   const todayDate: string = today.toISOString().split('T')[0];
 
   const dateRange: string[] = [];
+  
   for (let i = 0; i < 357; i++) {
     const date: Date = new Date(today);
     date.setDate(today.getDate() - i);
+
     dateRange.push(date.toISOString().split('T')[0]);
   }
 
   const cellStyle = (date: string) => {
     const isToday = date === todayDate;
+
     const count = contributions[date] || 0;
   
-    
+    let color = 'empty';
+    if (count >= 1 && count <= 9) {
+      color = 'lowContributions';
+
+} else if (count >= 10 && count <= 30) {
+      color = 'mediumContributions';
+    } else if (count > 30) {
+      color = 'highContributions';
+    }
+  
+    const cellClasses = [
+      'contribution-cell',
+      isToday ? 'today' : color,
+    ].join(' ');
+  
+    return cellClasses;
   };
   return (
-    <div className="contribution-graph">
-      <div className="day-label" />
-
-      {dateRange.map(date => (
-        <div
-          key={date}
-          className={cellStyle(date)}
-          data-count={contributions[date] || 0}
-        />
-      ))}
-    </div>
+    
+   <div className="cont" >
+          
+        <div className="contribution-graph">
+          
+      
+          {dateRange.map(date => (
+            <div
+              key={date}
+              className={cellStyle(date)}
+              data-count={contributions[date] || 0}
+            />
+          ))}
+        </div>
+   </div>
+   
   );
 };
 
