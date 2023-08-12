17a78b6b8a68de8842023c6370bec384a4f3e842
Islam Temirgaliev <islam2004tr@mail.ru>
2023-08-12 15:22:53 +0600

    analis

--- src/components/ContributionGraph/ContributionGraph.tsx
+++ src/components/ContributionGraph/ContributionGraph.tsx
@@ -15,32 +15,31 @@ const ContributionGraph: React.FC = () => {
         const data: ContributionData = await ky.get('https://dpg.gg/test/calendar.json').json();
         setContributions(data);
       } catch (error) {
-        console.error('ошибка при получении данных:', error);
+        console.error('Ошибка при получении данных:', error);
       }
     };
 
     fetchContributions();
   }, []);
 
-
-
   const today: Date = new Date();
-  const todayDate: string = today.toISOString().split('T')[0];
+  today.setHours(0, 0, 0, 0);
 
-  const dateRange: string[] = [];
-  for (let i = 0; i < 357; i++) {
-    const date: Date = new Date(today);
-    date.setDate(today.getDate() - i);
-    dateRange.push(date.toISOString().split('T')[0]);
+  const endDate: Date = new Date(today);
+  endDate.setDate(endDate.getDate() - 50 * 7);
+
+  const dateRange: Date[] = [];
+  for (let date = today; date >= endDate; date.setDate(date.getDate() - 1)) {
+    dateRange.push(new Date(date));
   }
 
-  const cellStyle = (date: string) => {
-    const isToday = date === todayDate;
-    const count = contributions[date] || 0;
-  
+  const cellStyle = (date: Date) => {
+    const isToday = date.toISOString() === today.toISOString();
+    const count = contributions[date.toISOString().split('T')[0]] || 0;
+
     let color = 'empty';
     let range = '';
-  
+
     if (count >= 1 && count <= 9) {
       color = 'lowContributions';
       range = '1-9';
@@ -51,38 +50,38 @@ const ContributionGraph: React.FC = () => {
       color = 'highContributions';
       range = '31+';
     }
-  
+
     const cellClasses = [
       'contribution-cell',
       isToday ? 'today' : color,
     ].join(' ');
-  
+
     return { cellClasses, range };
   };
-  
+
   return (
-    
-   <div className="cont" >
-          
-        <div className="contribution-graph">
-          
-       
-        {dateRange.map(date => {
-  const { cellClasses, range } = cellStyle(date);
-  return (
-    <div
-      key={date}
-      className={cellClasses}
-      data-count={contributions[date] || 0}
-    >
-      <span className="tooltip">{range}</span>
-    </div>
-  );
-})}
-         
+    <div className="cont">
+      <div className="contribution-graph">
+        <div className="days-of-week-row">
+          <div className="day-label" />
+          {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
+            <div key={day} className="day-label">{day}</div>
+          ))}
         </div>
-   </div>
-   
+        {dateRange.map(date => {
+          const { cellClasses, range } = cellStyle(date);
+          return (
+            <div
+              key={date.toISOString().split('T')[0]}
+              className={cellClasses}
+              data-count={contributions[date.toISOString().split('T')[0]] || 0}
+            >
+              <span className="tooltip">{range}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
