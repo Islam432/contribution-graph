0f79b5f1279d40eb1455e40645f40ca7dd51d3ab
Islam Temirgaliev <islam2004tr@mail.ru>
2023-08-12 14:58:20 +0600

    feat: style and html  change , to display diopozon

--- src/components/ContributionGraph/ContributionGraph.tsx
+++ src/components/ContributionGraph/ContributionGraph.tsx
@@ -22,33 +22,34 @@ const ContributionGraph: React.FC = () => {
     fetchContributions();
   }, []);
 
-
+//    const daysOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
   const today: Date = new Date();
   const todayDate: string = today.toISOString().split('T')[0];
 
   const dateRange: string[] = [];
-  
   for (let i = 0; i < 357; i++) {
     const date: Date = new Date(today);
     date.setDate(today.getDate() - i);
-
     dateRange.push(date.toISOString().split('T')[0]);
   }
 
   const cellStyle = (date: string) => {
     const isToday = date === todayDate;
-
     const count = contributions[date] || 0;
   
     let color = 'empty';
+    let range = '';
+  
     if (count >= 1 && count <= 9) {
       color = 'lowContributions';
-
-} else if (count >= 10 && count <= 30) {
+      range = '1-9';
+    } else if (count >= 10 && count <= 30) {
       color = 'mediumContributions';
+      range = '10-30';
     } else if (count > 30) {
       color = 'highContributions';
+      range = '31+';
     }
   
     const cellClasses = [
@@ -56,22 +57,32 @@ const ContributionGraph: React.FC = () => {
       isToday ? 'today' : color,
     ].join(' ');
   
-    return cellClasses;
+    return { cellClasses, range };
   };
+  
   return (
     
    <div className="cont" >
           
         <div className="contribution-graph">
           
-      
-          {dateRange.map(date => (
-            <div
-              key={date}
-              className={cellStyle(date)}
-              data-count={contributions[date] || 0}
-            />
-          ))}
+        {/* {daysOfWeek.map(day => (
+                <div key={day} className="day-label">{day}</div>
+              ))}
+           */}
+        {dateRange.map(date => {
+  const { cellClasses, range } = cellStyle(date);
+  return (
+    <div
+      key={date}
+      className={cellClasses}
+      data-count={contributions[date] || 0}
+    >
+      <span className="tooltip">{range}</span>
+    </div>
+  );
+})}
+         
         </div>
    </div>
    
